4863ba9cd2cf0a965c247d56cc9805882602afc7
opzarjun <arjun.y@opz0.io>
2023-09-15 19:28:00 +0530

    initial commit

---
+++ _example/example.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+  features {}
+}
+
+module "resource_group" {
+  source      = "git::git@github.com:opz0/terraform-azure-resource-group.git?ref=master"
+  name        = "appbastion"
+  environment = "tested"
+  location    = "North Europe"
+}
+
+module "vnet" {
+  source              = "git::git@github.com:opz0/terraform-azure-vnet.git?ref=master"
+  name                = "app"
+  environment         = "test"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  address_space       = "10.0.0.0/16"
+}
+
+module "name_specific_subnet" {
+  source = "git::git@github.com:opz0/terraform-azure-subnet.git?ref=master"
+
+  name                 = "app"
+  environment          = "test"
+  resource_group_name  = module.resource_group.resource_group_name
+  location             = module.resource_group.resource_group_location
+  virtual_network_name = module.vnet.vnet_name[0]
+
+  #subnet
+  specific_name_subnet  = true
+  specific_subnet_names = "AzureBastionSubnet"
+  subnet_prefixes       = ["10.0.1.0/24"]
+  # route_table
+  enable_route_table = false
+}
+
+module "bastion" {
+  depends_on          = [module.resource_group]
+  source              = "./../"
+  name                = "app"
+  environment         = "test"
+  resource_group_name = module.resource_group.resource_group_name
+  location            = module.resource_group.resource_group_location
+  subnet_id           = module.name_specific_subnet.specific_subnet_id
+}
